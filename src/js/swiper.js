d1365446aac269143e9666076185fc408772498d
MaksymSelezen <foqney@gmail.com>
2025-12-02 16:44:12 +0200

    end adaptive top nft's, explore, top sellers, subscribe, add breakpoint desktop swiper.js

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -6,15 +6,32 @@ import 'swiper/css/pagination';
 
 const topNftsSwiper = new Swiper('.top-nfts-swiper', {
   modules: [Pagination],
+
   slidesPerView: 1.1,
   spaceBetween: 25,
   pagination: {
     el: '.top-nfts-pagination',
     clickable: true,
   },
+
+  breakpoints: {
+    1600: {
+      slidesPerView: 'auto',
+      spaceBetween: 0,
+      allowTouchMove: false,
+    },
+  },
 });
 
 const exploreUsersSwiper = new Swiper('.explore-users-swiper', {
   slidesPerView: 1.1,
   spaceBetween: 25,
+
+  breakpoints: {
+    1600: {
+      slidesPerView: 'auto',
+      spaceBetween: 0,
+      allowTouchMove: false,
+    },
+  },
 });
